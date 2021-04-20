d398eb72e2e9e95e86e2cfb5d31061ffc2a04e7d
ANDREW-NB\numva <numv.andrew@gmail.com>
2021-04-20 09:19:18 +0300

    проект не работает

---
+++ лаба1.3.4- вариант 2/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace лаба1._3._4__вариант_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(33, 23);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(154, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Начать игру";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (254)));
+            this.richTextBox1.Location = new System.Drawing.Point(33, 66);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(154, 263);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(227, 23);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(178, 306);
+            this.richTextBox2.TabIndex = 2;
+            this.richTextBox2.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(443, 341);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.RichTextBox richTextBox2;
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+
+        #endregion
+    }
+}
---
+++ лаба1.3.4- вариант 2/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace лаба1._3._4__вариант_2
+{
+    public partial class Form1 : Form
+    {
+        public const int n = 15; //15 участников 
+        public int[] Raund = new int[15];
+        public bool[] loser = new bool[15];
+        public int winners = 0;
+        public int winner = 0;
+        public Random RandStep = new Random();
+        public bool isPlaying = false;
+
+        public int step;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.BackColor = Color.Chartreuse;
+            button1.Text = "Идёт игра";
+            //for (var i = 0; i < 15; i++) loser[i] = false;
+            
+            //for(var i = 1; i <= 15; i++) richTextBox1.AppendText(i + " игрок: \n");
+            
+            for (var j = 0; j < 14; j++) //14 заездов
+            {
+                
+                for (var i = 0; i < 15; i++)//чистка перед заездом
+                {
+                    //ends[i] = false;
+                    Raund[i] = 0;
+                }
+                winners = 0;
+                isPlaying = true;
+                
+                richTextBox2.AppendText("Новый заезд! \n");
+                Zaezd(j);
+            }
+            richTextBox2.AppendText("\n \n \n" + (winner+1) + " ПОБЕДИЛ!");
+        }
+
+        private void Zaezd(int j)
+        {
+            while (isPlaying) // пока идёт заезд 
+            {
+                for (var k = 0; k < 15; k++) // 15 игроков
+                {
+                    step = RandStep.Next(1, 6);
+                    if (Raund[k] < 100 && !loser[k] /*&& !ends[k] */)
+                    {
+                        Raund[k] += step;
+                        if (Raund[k] >= 100)
+                        {
+                            if (winners < 14)
+                            {
+                                richTextBox2.AppendText((k+1) + " игрок закончил заезд \n");
+                                winners += 1;
+                                //ends[k] = true;
+                            }
+                            else if (winners == 14)
+                            {
+                                richTextBox2.AppendText((k+1) + " игрок закончил заезд последний,\n Он выбывает \n");
+                                loser[k] = true;
+                                isPlaying = false;
+                            }
+
+                            /*if (j == 13)
+                            {
+                                
+                                winner = k;
+                            }*/
+                        }
+                        
+                    }
+                    
+                }
+            }
+        }
+        
+        
+    }
+}
